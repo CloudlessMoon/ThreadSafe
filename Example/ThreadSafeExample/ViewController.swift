a5a6c3a5160ba4d1994a53e3750aab2c728d1181
jiasong <593908937@qq.com>
2024-05-30 14:25:00 +0800

    SwiftLint

--- Example/ThreadSafeExample/ViewController.swift
+++ Example/ThreadSafeExample/ViewController.swift
@@ -6,14 +6,107 @@
 //
 
 import UIKit
+import ThreadSafe
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let readWriteTask = ReadWriteTask(label: "test")
+        
+        var name = "0"
+        
+        let count = 10
+        for item in 1...count {
+            DispatchQueue.global().async {
+                name = "1"
+                
+                let name1 = readWriteTask.read { name }
+                print("readWriteTask1 read1 \(name1)")
+                
+                readWriteTask.write {
+                    name = "\(item)"
+                    print("readWriteTask1 write1 \(name)")
+                    
+                    let name1 = readWriteTask.read { name }
+                    print("readWriteTask1 read1 \(name1)")
+                    
+                    readWriteTask.write {
+                        name = "22_\(item)"
+                        print("readWriteTask1 write2 \(name)")
+                        
+                        readWriteTask.write {
+                            name = "22_\(item)"
+                            print("readWriteTask1 write2 \(name)")
+                            
+                            let name1 = readWriteTask.read { name }
+                            print("readWriteTask1 read1 \(name1)")
+                        }
+                        
+                        let name1 = readWriteTask.read { name }
+                        print("readWriteTask1 read1 \(name1)")
+                    }
+                }
+                
+                let name2 = readWriteTask.read {
+                    return name
+                }
+                print("readWriteTask1 read \(name2)")
+            }
+            
+            DispatchQueue.global().async {
+                print("readWriteTask1 read1 \(name)")
+                
+                let name1 = readWriteTask.read {
+                    //                    let name2 = readWriteTask.write {
+                    //                        return name
+                    //                    }
+                    return name
+                }
+                print("readWriteTask1 read \(name1)")
+                
+                let name2 = readWriteTask.read {
+                    return name
+                }
+                print("readWriteTask1 read \(name2)")
+                
+                let name3 = readWriteTask.read {
+                    return name
+                }
+                print("readWriteTask1 read \(name3)")
+                
+                let name4 = readWriteTask.read {
+                    return name
+                }
+                print("readWriteTask1 read \(name4)")
+                
+                readWriteTask.write {
+                    name = "\(item)"
+                    print("readWriteTask1 write1 \(name)")
+                    
+                    let name1 = readWriteTask.read { name }
+                    print("readWriteTask1 read1 \(name1)")
+                    
+                    readWriteTask.write {
+                        name = "22_\(item)"
+                        print("readWriteTask1 write2 \(name)")
+                        
+                        readWriteTask.write {
+                            name = "22_\(item)"
+                            print("readWriteTask1 write2 \(name)")
+                            
+                            let name1 = readWriteTask.read { name }
+                            print("readWriteTask1 read1 \(name1)")
+                        }
+                    }
+                    
+                    print("readWriteTask1 write1 \(name)")
+                }
+                
+                let name5 = readWriteTask.read { name }
+                print("readWriteTask1 read1 \(name5)")
+            }
+        }
     }
 
-
 }
-
--- Example/ThreadSafeExampleTests/ThreadSafeExampleTests.swift
+++ Example/ThreadSafeExampleTests/ThreadSafeExampleTests.swift
@@ -12,112 +12,20 @@ import ThreadSafe
 final class ThreadSafeExampleTests: XCTestCase {
     
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+       
     }
 
     func testExample() throws {
-        let readWriteTask = ReadWriteTask(label: "test")
-        
-        var name: String = "0"
-        
-        let count = 10
-        for item in 1...count {
-            DispatchQueue.global().async {
-                let name1 = readWriteTask.read { name }
-                print("readWriteTask1 read1 \(name1)")
-                
-                readWriteTask.write {
-                    name = "\(item)"
-                    print("readWriteTask1 write1 \(name)")
-                    
-                    let name1 = readWriteTask.read { name }
-                    print("readWriteTask1 read1 \(name1)")
-                    
-                    readWriteTask.write {
-                        name = "22_\(item)"
-                        print("readWriteTask1 write2 \(name)")
-                        
-                        readWriteTask.write {
-                            name = "22_\(item)"
-                            print("readWriteTask1 write2 \(name)")
-                            
-                            let name1 = readWriteTask.read { name }
-                            print("readWriteTask1 read1 \(name1)")
-                        }
-                        
-                        let name1 = readWriteTask.read { name }
-                        print("readWriteTask1 read1 \(name1)")
-                    }
-                }
-                
-                let name2 = readWriteTask.read {
-                    return name
-                }
-                print("readWriteTask1 read \(name2)")
-            }
-            
-            DispatchQueue.global().async {
-                let name1 = readWriteTask.read {
-                    //                    let name2 = readWriteTask.write {
-                    //                        return name
-                    //                    }
-                    return name
-                }
-                print("readWriteTask1 read \(name1)")
-                
-                let name2 = readWriteTask.read {
-                    return name
-                }
-                print("readWriteTask1 read \(name2)")
-                
-                let name3 = readWriteTask.read {
-                    return name
-                }
-                print("readWriteTask1 read \(name3)")
-                
-                let name4 = readWriteTask.read {
-                    return name
-                }
-                print("readWriteTask1 read \(name4)")
-                
-                readWriteTask.write {
-                    name = "\(item)"
-                    print("readWriteTask1 write1 \(name)")
-                    
-                    let name1 = readWriteTask.read { name }
-                    print("readWriteTask1 read1 \(name1)")
-                    
-                    readWriteTask.write {
-                        name = "22_\(item)"
-                        print("readWriteTask1 write2 \(name)")
-                        
-                        readWriteTask.write {
-                            name = "22_\(item)"
-                            print("readWriteTask1 write2 \(name)")
-                            
-                            let name1 = readWriteTask.read { name }
-                            print("readWriteTask1 read1 \(name1)")
-                        }
-                    }
-                    
-                    print("readWriteTask1 write1 \(name)")
-                }
-                
-                let name5 = readWriteTask.read { name }
-                print("readWriteTask1 read1 \(name5)")
-            
-            }
-        }
+       
     }
 
     func testPerformanceExample() throws {
-        // This is an example of a performance test case.
         self.measure {
-            // Put the code you want to measure the time of here.
+       
         }
     }
 
